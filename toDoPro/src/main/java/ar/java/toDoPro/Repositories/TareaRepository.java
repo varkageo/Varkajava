214f7dd3b699a0db0a08eeb77564303cbde15370
Java <Java@NC0583>
2019-11-15 11:56:17 -0300

    llllllllllllllll

--- toDoPro/src/main/java/ar/java/toDoPro/Repositories/TareaRepository.java
+++ toDoPro/src/main/java/ar/java/toDoPro/Repositories/TareaRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import ar.java.toDoPro.Models.*;
 
 public interface TareaRepository extends CrudRepository <Tarea, Integer> {
+	
+	Iterable<Tarea> findByNombre(String nombre);
+	Iterable<Tarea> findByNombreContains(String nombre);
 
 }
